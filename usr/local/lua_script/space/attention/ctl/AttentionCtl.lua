f13e7800afbf4257d5587915092fe5ca978b3aea
zh <lyzhanghai@sina.cn>
2015-07-08 09:39:05 +0800

    a

---
+++ usr/local/lua_script/space/attention/ctl/AttentionCtl.lua
@@ -0,0 +1,128 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: 张海
+-- Date: 2015/7/6
+-- Time: 9:45
+-- To change this template use File | Settings | File Templates.
+--
+
+ngx.header.content_type = "text/plain";
+local web = require("social.router.web")
+local cjson = require "cjson"
+local request = require("social.common.request")
+local no_permission_context = ngx.var.path_uri_no_permission --无权限的context.
+local context = ngx.var.path_uri --有权限的context.
+local log = require("social.common.log")
+local service = require("space.attention.service.AttentionService")
+
+-------------------------------------------------------------------------------------------------------
+-- 保存关注信息.
+local function save()
+    local personid = request:getStrParam("personid", true, true) --关注人id
+    local identityid = request:getStrParam("identityid", true, true) --关注人id
+    local b_personid = request:getStrParam("b_personid", true, true) --被关注人id
+    local b_identityid = request:getStrParam("b_identityid", true, true) --被关注人的身份.
+    local r = service.save({ personid = personid, identityid = identityid, b_personid = b_personid, b_identityid = b_identityid })
+    local result = {}
+    result.success = true;
+    if not r then
+        result.success = false;
+        result.info = { name = "", data = "添加出错." }
+        ngx.say(cjson.encode(result));
+        return;
+    end
+    ngx.say(cjson.encode(result));
+end
+
+-------------------------------------------------------------------------------------------------------
+-- 查询关注信息.
+local function query()
+    log.debug("查询关注信息.")
+    local personid = request:getStrParam("personid", true, true) --关注人id
+    local identityid = request:getStrParam("identityid", true, true) --关注人id
+    local b_personid = request:getStrParam("b_personid", true, true) --被关注人id
+    local b_identityid = request:getStrParam("b_identityid", true, true) --被关注人的身份.
+    local result = { success = true, list = {} }
+    local list = service.queryAttention({ personid = personid, identityid = identityid, b_personid = b_personid, b_identityid = b_identityid })
+    if not list then
+        ngx.say(cjson.encode({ success = false }))
+        return;
+    end
+    result.list = list
+    ngx.say(cjson.encode(result));
+end
+
+-------------------------------------------------------------------------------------------------------
+-- 查询被关注信息.
+local function bquery()
+    log.debug("查询被关注信息.")
+    local personid = request:getStrParam("personid", true, true) --关注人id
+    local identityid = request:getStrParam("identityid", true, true) --关注人id
+    local b_personid = request:getStrParam("b_personid", true, true) --被关注人id
+    local b_identityid = request:getStrParam("b_identityid", true, true) --被关注人的身份.
+    local result = { success = true, list = {} }
+    local list = service.queryBAttention({ personid = personid, identityid = identityid, b_personid = b_personid, b_identityid = b_identityid })
+    if not list then
+        ngx.say(cjson.encode({ success = false }))
+        return;
+    end
+    result.list = list
+    ngx.say(cjson.encode(result));
+end
+
+local function get()
+    local personid = request:getStrParam("personid", false, true) --关注人id
+    local identityid = request:getStrParam("identityid", false, true) --关注人id
+    local b_personid = request:getStrParam("b_personid", true, true) --被关注人id
+    local b_identityid = request:getStrParam("b_identityid", true, true) --被关注人的身份.
+    local result = service.get({ personid = personid, identityid = identityid, b_personid = b_personid, b_identityid = b_identityid })
+    if not result then
+        result.success = false
+        ngx.say(cjson.encode(result))
+        return;
+    end
+    ngx.say(cjson.encode(result));
+end
+
+------------------------------------------------------------------------------------------------------
+-- 设置访问量.
+local function access()
+    local b_personid = request:getStrParam("b_personid", true, true) --被关注人id
+    local b_identityid = request:getStrParam("b_identityid", true, true) --被关注人的身份.
+    local result = service.access(b_personid, b_identityid)
+    if not result then
+        ngx.say(cjson.encode({ success = false }))
+        return;
+    end
+    ngx.say(cjson.encode({ success = true }))
+end
+
+
+------------------------------------------------------------------------------------------------------
+-- 取消关注
+local function delete()
+    local personid = request:getStrParam("personid", true, true) --关注人id
+    local identityid = request:getStrParam("identityid", true, true) --关注人id
+    local b_personid = request:getStrParam("b_personid", true, true) --被关注人id
+    local b_identityid = request:getStrParam("b_identityid", true, true) --被关注人的身份.
+    local r = service.delete({ personid = personid, identityid = identityid, b_personid = b_personid, b_identityid = b_identityid })
+    if not r then
+        ngx.say(cjson.encode({ success = false }))
+        return;
+    end
+    ngx.say(cjson.encode({ success = true }))
+end
+
+
+-- 配置url.
+-- 按功能分
+local urls = {
+    context .. '/save', save,
+    context .. '/query', query,
+    context .. '/bquery', bquery,
+    no_permission_context .. '/get', get,
+    no_permission_context .. '/access', access,
+    context .. '/cancel', delete
+}
+local app = web.application(urls, nil)
+app:start()
---
+++ usr/local/lua_script/space/attention/service/AttentionService.lua
@@ -0,0 +1,181 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: 张海 .
+-- Date: 2015/7/6
+-- Time: 9:46
+-- To change this template use File | Settings | File Templates.
+--
+
+local log = require("social.common.log")
+local RedisUtil = require("social.common.redisutil")
+local SsdbUtil = require("social.common.ssdbutil")
+local TS = require "resty.TS"
+local _M = {}
+
+--------------------------------------------------------------------
+local function checkParamIsNull(t)
+    for key, var in pairs(t) do
+        if var == nil or string.len(var) == 0 then
+            error(key .. " 不能为空.")
+        end
+    end
+end
+
+--设置关注
+local function setAttention(param)
+    local db = SsdbUtil:getDb();
+    local key = param.b_identityid .. "_" .. param.b_personid
+    db:zset("space_attention_identityid_" .. param.identityid .. "_personid_" .. param.personid, key, TS.getTs())
+    db:incr("space_attention_identityid_" .. param.identityid .. "_personid_" .. param.personid .. "_count", 1);
+end
+
+--设置被关注.
+local function setBAttention(param)
+    local db = SsdbUtil:getDb();
+    local key = param.identityid .. "_" .. param.personid
+    db:zset("space_b_attention_identityid_" .. param.b_identityid .. "_personid_" .. param.b_personid, key, TS.getTs())
+    db:incr("space_b_attention_identityid_" .. param.b_identityid .. "_personid_" .. param.b_personid .. "_count", 1);
+end
+
+-------------------------------------------------------------
+-- 保存关注人信息.
+function _M.save(param)
+    checkParamIsNull(param)
+    local status = pcall(function()
+        setAttention(param)
+        setBAttention(param)
+    end)
+    if status then
+        return true;
+    end
+    return false;
+end
+
+local function getPersonInfoByRedis(zResult)
+    local redisDb = RedisUtil:getDb()
+    local result = {}
+    if zResult and zResult[1] and zResult[1] ~= "ok" then
+        for i = 1, #zResult, 2 do
+            local temp = {}
+            local r = Split(zResult[i], "_")
+            local _personid = r[2];
+            local _identityid = r[1];
+            local personA = redisDb:hmget("person_" .. _personid .. "_" .. _identityid, "person_name", "avatar_url");
+
+            local person_name = personA[1]
+            local avatar_url = personA[2]
+            log.debug(person_name)
+            log.debug(avatar_url)
+            temp.personid = _personid;
+            temp.identityid = _identityid
+            temp.person_name = person_name;
+            temp.avatar_url = avatar_url;
+            table.insert(result, temp);
+        end
+    end
+    return result;
+end
+
+local function getAttention(personid, identityid)
+    local db = SsdbUtil:getDb();
+    local zResult = db:zrange("space_attention_identityid_" .. identityid .. "_personid_" .. personid, 0, 10)
+    log.debug(zResult);
+    local result = getPersonInfoByRedis(zResult)
+    return result;
+end
+
+local function getBAttention(personid, identityid)
+    local db = SsdbUtil:getDb();
+    local name = "space_b_attention_identityid_" .. identityid .. "_personid_" .. personid;
+    local zResult = db:zrange(name, 0, 10)
+    log.debug(name)
+    log.debug(zResult);
+    local result = getPersonInfoByRedis(zResult)
+    return result;
+end
+
+--------------------------------------------------------------
+-- 查询关注人
+function _M.queryAttention(param)
+    checkParamIsNull({
+        personid = param.personid,
+        identityid = param.identityid,
+        b_personid = param.b_personid,
+        b_identityid = param.b_identityid,
+    })
+    local result = getAttention(param.personid, param.identityid)
+    return result
+end
+
+--------------------------------------------------------------
+-- 查询关注人
+function _M.queryBAttention(param)
+    log.debug(param)
+    checkParamIsNull({
+        personid = param.personid,
+        identityid = param.identityid,
+        b_personid = param.b_personid,
+        b_identityid = param.b_identityid,
+    })
+    local result = getBAttention(param.personid, param.identityid)
+    return result
+end
+
+
+function _M.get(param)
+    local result = {}
+    --    checkParamIsNull({
+    --        personid = personid,
+    --        identityid = identityid,
+    --    })
+    local db = SsdbUtil:getDb();
+    if param.personid and param.identityid then
+        local attention_count = db:get("space_attention_identityid_" .. param.identityid .. "_personid_" .. param.personid .. "_count"); --关注数量
+        if attention_count and attention_count[1] and string.len(attention_count[1]) > 0 then
+            result.attention_count = attention_count[1];
+        else
+            result.attention_count = 0
+        end
+        local attentionb_count = db:get("space_b_attention_identityid_" .. param.identityid .. "_personid_" .. param.personid .. "_count"); --被关注数量
+        if attentionb_count and attentionb_count[1] and string.len(attentionb_count[1]) > 0 then
+            result.attentionb_count = attentionb_count[1];
+        else
+            result.attentionb_count = 0
+        end
+        local is_attention = db:zexists("space_attention_identityid_" .. param.identityid .. "_personid_" .. param.personid, param.b_identityid .. "_" .. param.b_personid)
+        log.debug(is_attention)
+        if is_attention and is_attention[1] and tonumber(is_attention[1])>0 then
+            result.is_attention = 1;
+        else
+            result.is_attention = 0;
+        end
+    end
+    local access_quantity = db:get("space_attention_access_quantity" .. param.b_identityid .. "_personid_" .. param.b_personid)
+    if access_quantity and access_quantity[1] and string.len(access_quantity[1]) > 0 then
+        result.access_quantity = access_quantity[1]
+    else
+        result.access_quantity = 0
+    end
+    return result;
+end
+
+function _M.access(b_personid, b_identityid)
+    local db = SsdbUtil:getDb();
+    local result = db:incr("space_attention_access_quantity" .. b_identityid .. "_personid_" .. b_personid, 1);
+    return result;
+end
+
+
+function _M.delete(param)
+    checkParamIsNull(param)
+    local db = SsdbUtil:getDb();
+    local key = param.b_identityid .. "_" .. param.b_personid
+    db:zdel("space_attention_identityid_" .. param.identityid .. "_personid_" .. param.personid, key)
+    db:incr("space_attention_identityid_" .. param.identityid .. "_personid_" .. param.personid .. "_count", -1);
+    local b_key = param.identityid .. "_" .. param.personid
+    db:zdel("space_b_attention_identityid_" .. param.b_identityid .. "_personid_" .. param.b_personid, b_key)
+    db:incr("space_b_attention_identityid_" .. param.b_identityid .. "_personid_" .. param.b_personid .. "_count", -1);
+    return true;
+end
+
+return _M;
